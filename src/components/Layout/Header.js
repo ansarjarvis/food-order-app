b1ac18f4f906a69e44daac642dcc14e2806837c0
ansarjarvis <ansar.ofcl00@gmail.com>
2022-02-04 00:21:13 +0530

    add meals and layout components

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton></HeaderCartButton>
+      </header>
+      <div className={classes["main-image"]}>
+        <img
+          src="https://github.com/academind/react-complete-guide-code/blob/11-practice-food-order-app/extra-files/meals.jpg?raw=true"
+          alt="a delicious meal for delicious peoples "
+        ></img>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
